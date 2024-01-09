19736e15730aaff2957144b66f922cce0f318c2b
Talha Akhtar <talhaakhtarkpr1@gmail.com>
2024-01-09 22:18:44 +0500

    Commit

---
+++ app.js
@@ -0,0 +1,46 @@
+const express=require('express');
+const path=require('path');
+const bodyParser=require('body-parser');
+const cors=require('cors');
+const passport=require('passport');
+const mongoose=require('mongoose');
+const config=require('./config/database')
+
+mongoose.connect(config.database)
+
+mongoose.connection.on('connected',()=>{
+    console.log(`Connected to DB ${config.database}` )
+})
+
+const port=3000;
+
+//App
+const app=express();
+
+const users=require("./routes/users")
+
+//CORS
+app.use(cors());
+
+
+
+//static folder
+app.use(express.static(path.join(__dirname,'public')))
+//Body Part MiddleWare
+app.use(bodyParser.json());
+
+
+app.use('/users',users)
+
+
+
+//Index Route
+app.get('/',(req,res)=>{
+    res.send("INVALID EndPoint")
+
+})
+
+//Start Server
+app.listen(port,()=>{
+    console.log(`Server start at port ${port}`);
+})
---
+++ routes/users.js
@@ -0,0 +1,19 @@
+// users.js
+
+const express = require('express');
+const router = express.Router();
+
+router.get('/register', (req, res, next) => {
+    res.send('REGISTER');
+});
+router.get('/authenticate', (req, res, next) => {
+    res.send('AUTHENTICATE');
+});
+router.get('/profile', (req, res, next) => {
+    res.send('Profile');
+});
+router.get('/validate', (req, res, next) => {
+    res.send('Validation');
+});
+
+module.exports = router;
