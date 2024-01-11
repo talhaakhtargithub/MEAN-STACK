d41d414b3ca326ec6d8ec7cf7ef691c1192de3c2
Talha Akhtar <talhaakhtarkpr1@gmail.com>
2024-01-12 00:23:50 +0500

    Register Done

--- models/user.js
+++ models/user.js
@@ -1,33 +1,47 @@
-const mongoose=require('mongoose');
-const bcrypt=require('bcryptjs');
-const config=require('../config/database');
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+const config = require('../config/database');
 
-
-
-const UserSchema=mongoose.Schema({
-    name:{
-        type:String
+const UserSchema = mongoose.Schema({
+    name: {
+        type: String
     },
-    email:{
-        type:String,
-        required:true
+    email: {
+        type: String,
+        required: true
     },
-    password:{
-        type:String,
-        required:true
+    password: {
+        type: String,
+        required: true
     }
 });
 
+const User = module.exports = mongoose.model('User', UserSchema);
 
+module.exports.getUserById = function (id, callback) {
+    User.findById(id, callback);
+};
 
-const User=module.exports=mongoose.model('User',UserSchema);
+module.exports.getUserByUsername = function (username, callback) {
+    const query = { username: username };
+    User.findOne(query, callback);
+};
 
-module.exports.getUserById=function(id,callback) {
-    User.findById(id,callback)
-    
-}
-module.exports.getUserById=function(username,callback) {
-    const query={username:username}
-    User.findOne(query,callback)
-    
-}
+module.exports.addUser = function (newUser, callback) {
+    bcrypt.genSalt(10, (err, salt) => {
+        if (err) throw err;
+
+        bcrypt.hash(newUser.password, salt, (err, hash) => {
+            if (err) throw err;
+
+            newUser.password = hash;
+            newUser.save()
+                .then(user => {
+                    callback(null, user); // Invoke callback with no error and the saved user
+                })
+                .catch(err => {
+                    callback(err, null); // Invoke callback with the error
+                });
+        });
+    });
+};
