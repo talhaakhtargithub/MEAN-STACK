14176ffc3bea08a4f8d7904e1fbd8917194c9778
Talha Akhtar <talhaakhtarkpr1@gmail.com>
2024-01-18 17:19:46 +0500

    Sync

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const cors=require('cors');
 const passport=require('passport');
 const mongoose=require('mongoose');
 const config=require('./config/database')
-
+const users=require("./routes/users")
 mongoose.connect(config.database)
 
 mongoose.connection.on('connected',()=>{
@@ -20,7 +20,7 @@ const port=3000;
 //App
 const app=express();
 
-const users=require("./routes/users")
+
 
 //CORS
 app.use(cors());
